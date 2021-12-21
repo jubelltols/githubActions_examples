d594948c7cec30e299380d21901c13632f160352
jubelltols <jubelltols@gmail.com>
2021-12-21 15:47:35 +0100

    add jenkinsfile

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const port = 3000;
+const PORT = 3001;
+const memeLib = require('nodejs-meme-generator');
 
-app.get("/", (req, res) => {
-  res.send("Hello World!!");
+const memeGenerator = new memeLib({
+  canvasOptions: { // optional
+    canvasWidth: 500,
+    canvasHeight: 500
+  },
+  fontOptions: { // optional
+    fontSize: 46,
+    fontFamily: 'impact',
+    lineHeight: 2
+  }
 });
 
-app.listen(port, () => {
-  console.log("Example app listening at http://localhost:3000");
+app.get('/', function (req, res) {
+  memeGenerator.generateMeme({
+      // you can use either topText or bottomText
+      // or both of them at the same time
+      topText: 'Meme',
+      bottomText: 'Generator',
+      url: 'https://i.imgur.com/7FHoSIG.png'
+    })
+    .then(function(data) {
+      res.set('Content-Type', 'image/png');
+      res.send(data);
+    })
 });
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
